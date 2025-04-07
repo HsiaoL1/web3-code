328bda26f1a5769d63b7c0d2c5fdfbc977ed1e1a
HsiaoL1 <shawdml5@gmail.com>
2025-04-07 09:32:56 +0800

    add some

--- book/main.go
+++ book/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -31,4 +35,32 @@ func main() {
 	)
 
 	// Middleware
+	// 启动服务器
+	go func() {
+		if err := app.Listen(":" + port); err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Fatal("Failed to start server")
+		}
+	}()
+
+	// 优雅关机
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit // 等待信号
+
+	logrus.Info("Shutting down server...")
+
+	// 设置超时时间
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Error shutting down server")
+	}
+
+	logrus.Info("Server gracefully stopped")
 }
